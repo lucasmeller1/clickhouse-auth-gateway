c5ab0ebeb4915da3c2247d4b772fc6b8ebf72ee2
lucasmeller <lucasmellermoto@gmail.com>
2026-02-24 23:29:54 -0300

    removed some meters in middleware/auth, changed tempo config

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"log"
 	"net/http"
-	"time"
 
 	"github.com/lucasmeller1/excel_api/internal/auth"
 	"github.com/lucasmeller1/excel_api/internal/config"
@@ -15,43 +13,17 @@ import (
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/metric"
 )
 
 const name = "github.com/lucasmeller1/excel_api/internal/middleware"
 
 var (
-	meter              = otel.Meter(name)
-	tracer             = otel.Tracer(name)
-	authDuration       metric.Float64Histogram
-	numberUserRequests metric.Int64Counter
+	tracer = otel.Tracer(name)
 )
-
-func init() {
-	var err error
-
-	authDuration, err = meter.Float64Histogram(
-		"auth.validation.duration",
-		metric.WithDescription("Time spent validating JWT tokens."),
-		metric.WithUnit("s"),
-	)
-	if err != nil {
-		log.Printf("failed to create metric: %v\n", err)
-	}
-
-	numberUserRequests, err = meter.Int64Counter(
-		"number.user.requests",
-		metric.WithDescription("Number of requests per user."),
-	)
-	if err != nil {
-		log.Printf("failed to create metric: %v\n", err)
-	}
-}
 
 func AuthPublicMiddleware(cfg config.AuthConfig, redisClient *redis.RedisClient) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			start := time.Now()
 			ctx := r.Context()
 
 			ctx, span := tracer.Start(ctx, "Middleware.Public.Auth")
@@ -89,16 +61,6 @@ func AuthPublicMiddleware(cfg config.AuthConfig, redisClient *redis.RedisClient)
 			if labeler, ok := otelhttp.LabelerFromContext(ctx); ok {
 				labeler.Add(attribute.String("user.oid", claims.OID))
 			}
-
-			authDuration.Record(ctx, time.Since(start).Seconds())
-
-			numberUserRequests.Add(
-				ctx,
-				1,
-				metric.WithAttributes(
-					attribute.String("oid", claims.OID),
-				),
-			)
 
 			// if cfg.Debug == "1" {
 			// 	log.Println(bearerToken)
